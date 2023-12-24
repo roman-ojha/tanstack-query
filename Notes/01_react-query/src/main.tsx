114e793f41ce19690deffbf5648afdb567c2e22d
Roman-Ojha <razzroman98@gmail.com>
2023-12-24 10:03:52 +0545

    feat: init react app using vite for react-query

---
+++ Notes/01_react-query/src/main.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Products from "./Products";
+import Product from "./Product";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/products/:productId",
+    element: <Product />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
